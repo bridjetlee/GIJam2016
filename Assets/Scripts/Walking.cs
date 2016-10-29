81070f3dda4772dd473dea6098504a1021f8e495
bridjetlee <bridjet.lee@gmail.com>
2016-10-29 18:35:53 -0400

    Attempt at merging animation and motion
    Didn’t work

--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -5,13 +5,18 @@ public class Walking : MonoBehaviour {
 
     public Vector2 velocity;
     public Rigidbody2D rb2D;
-    
+	Animator animator;
 
     void Start () {
         rb2D = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator> ();
         transform.position.Set(1, 1, 0);
     }
-
+	void Walking () {
+		var walking = animator.GetBool("Walking-new");
+		if (walking == 1){
+			animation.Play("Walking-new");
+		}
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.RightArrow))
