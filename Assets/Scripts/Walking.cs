54f86fbbd3b5a9533bd0a95b58768f421819abb9
LemonTrees7 <vincent.koo.vk@gmail.com>
2016-10-30 15:13:15 -0400

    Collision course
    Requires trigger on pumpkins

--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -1,31 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Walking : MonoBehaviour {
+public class Walking : MonoBehaviour
+{
 
-    public Vector2 velocity;
-    public Rigidbody2D rb2D;
-	Animator animator;
+    float speed = 10.0f;
 
-    void Start () {
-        rb2D = GetComponent<Rigidbody2D>();
-		animator = GetComponent<Animator> ();
-        transform.position.Set(1, 1, 0);
-    }
-	void Walking () {
-		var walking = animator.GetBool("Walking-new");
-		if (walking == 1){
-			animation.Play("Walking-new");
-		}
-    void FixedUpdate()
+    void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb2D.MovePosition(rb2D.position - velocity * Time.fixedDeltaTime);
-        }    
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    { Destroy(other.gameObject); }
 }
